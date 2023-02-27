ac0ed733d882a503ddc387645f982fad09b4d827
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-02-27 10:58:51 -0300

    NounPhraseExtraction

---
+++ NounPhrasesExtraction.py
@@ -0,0 +1,13 @@
+from textblob import TextBlob
+print("CASO A")
+blob = TextBlob("John is learning natural language processing")
+for np in blob.noun_phrases:
+    print(np)
+#Ejemplo mas largo
+print("CASO B")
+with open('./Datos/Agricultura.txt',"r",encoding='utf-8') as f:
+    contents = f.read()
+    #print(contents)
+    agriculture = TextBlob(contents)
+    for np in agriculture.noun_phrases:
+        print(np)
