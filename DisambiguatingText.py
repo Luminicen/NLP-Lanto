eeb1b3208bf80e5ad111c9068fbfeaf16bbd92b7
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-02-27 13:17:03 -0300

    Desambiguar texto

---
+++ DisambiguatingText.py
@@ -0,0 +1,16 @@
+from nltk.corpus import wordnet as wn
+from nltk.stem import PorterStemmer
+from itertools import chain
+from pywsd.lesk import simple_lesk
+# Sentences
+bank_sents = ['I went to the bank to deposit my money',
+'The river bank was full of dead fishes']
+# calling the lesk function and printing results for both the sentences
+print ("Context-1:", bank_sents[0])
+answer = simple_lesk(bank_sents[0],'bank')
+print ("Sense:", answer)
+print ("Definition : ", answer.definition())
+print ("Context-2:", bank_sents[1])
+answer = simple_lesk(bank_sents[1],'bank','n')
+print ("Sense:", answer)
+print ("Definition : ", answer.definition())
