e0fa69709a9a99461722d2207ba46feed0a2fa34
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-03-03 12:37:39 -0300

    SentimentAnalisis

--- SentimentAnalisis.py
+++ SentimentAnalisis.py
@@ -57,3 +57,25 @@ print("REVIEW 2")
 blob = TextBlob(review2_limpio)
 print(blob.sentiment)
 #cuanto mas se acerque a 1 es mas positiva y cuanto mas se acerque a -1 es negativa
+#ejemplos mas largos
+review_larga_positiva="""Total War: Warhammer 3 is the perfect example of a sequel done well. All the elements of both Total War and Warhammer universe fused into a game of epic proportions.
+ Some innovation in the variety of missions as well bring a bit more freshness in the gameplay. 
+ Graphics as usual stunning, with scenery that looks like fresh."""
+review_larga_neutral="""Warhammer 3 opens strongly. The narrative hook of the prologue sinks deep and the raft of tweaks to the strategic layer and tactical battles are all welcome. But it can't sustain the early momentum. The endgame objectives feel like a distraction, even though they're the main point, and serve only to diminish the entire campaign. The factions all have different reasons for wanting the endgame MacGuffin, but none of those motivations make a difference to how the campaign plays out. They're all trapped in the same Chaos Realm, going through the same motions, in pursuit of the same unsatisfying win conditions. In the end, Total War: Warhammer 3 is a good game--there just isn't a good reason to see it through to the bitter end."""
+review_larga_negativa=""
+with open('./Datos/review_negativa.txt',"r",encoding='utf-8') as f:
+    contents = f.read()
+    review_larga_negativa=contents
+positiva = processRow(review_larga_positiva)
+neutral = processRow(review_larga_neutral)
+negativa = processRow(review_larga_negativa)
+blob = TextBlob(positiva)
+print("REVIEW POSITIVA")
+print(blob.sentiment)
+blob = TextBlob(neutral)
+print("REVIEW NEUTRAL")
+print(blob.sentiment)
+blob = TextBlob(negativa)
+print("REVIEW NEGATIVA")
+print(blob.sentiment)
+
