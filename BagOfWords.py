94c6706d664dd70f32ca47b3be12f29ff551d877
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-03-13 10:28:04 -0300

    reacomodo comentarios y print

--- BagOfWords.py
+++ BagOfWords.py
@@ -7,19 +7,17 @@ sentences = [['I', 'love', 'nlp'],
  ['nlp', 'saves', 'time', 'and', 'solves', 
 'lot', 'of', 'industry', 'problems'],
  ['nlp', 'uses', 'machine', 'learning']]
+#creo el modelo y lo configuro
 cbow = Word2Vec(sentences, vector_size =50, window = 3, min_count=1,sg = 1)
-print(cbow)
-print(cbow.wv['nlp'])
-# save model
 cbow.save('cbow.bin')
-# load model
 cbow = Word2Vec.load('cbow.bin')
 X = cbow.wv[cbow.wv.index_to_key]
 pca = PCA(n_components=2)
 result = pca.fit_transform(X)
-# create a scatter plot of the projection
+#configuraciones para crear el plot
 pyplot.scatter(result[:, 0], result[:, 1])
 words = list(cbow.wv.index_to_key)
+print("cuanto mas juntas esten las palabras en el grafico mas relacionadas estan en el contexto del documento")
 for i, word in enumerate(words):
  pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
 pyplot.show()
--- bigram_based-features.py
+++ bigram_based-features.py
@@ -1,8 +1,12 @@
 from sklearn.feature_extraction.text import CountVectorizer
-# Text
+# ejemplo
 text = ["I love NLP and I will learn NLP in 2month "]
+#generamos un vector con la cantidad de ocurrencias de las palabras
 vectorizer = CountVectorizer(ngram_range=(2,2))
+#entrenamos
 vectorizer.fit(text)
+#pasamos el texto por el vectorizador
 vector = vectorizer.transform(text)
+print("Bigramas encontrados")
 print(vectorizer.vocabulary_)
-print(vector.toarray())
+#print(vector.toarray())
