bc923e83f70ebec28e7f8ca5e300a0c0bb15458f
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-09-29 11:50:57 -0300

    Update TF-IDF.py

--- TF-IDF.py
+++ TF-IDF.py
@@ -7,5 +7,8 @@ vectorizer = TfidfVectorizer()
 #Tokenize and build vocab
 vectorizer.fit(Text)
 #Summarize
+feature_names = vectorizer.get_feature_names_out()
 print(vectorizer.vocabulary_)
 print(vectorizer.idf_)
+for word, idf_value in zip(feature_names, vectorizer.idf_):
+    print(f'{word}: {idf_value}')
